a91c8ae442b9621a0b1529d5d438a06b73a0eb9d
Jonathan Tan <Jonalogy@Jonathans-MacBook-Air.local>
2016-08-27 17:51:08 +0800

    Debugged checkWin, included new function called body to all other game logic functions and later renamed to gameButler. Game works fine, yet to test out AI

--- logic.js
+++ logic.js
@@ -13,21 +13,45 @@ document.getElementById('ai').addEventListener('click',ai);
 
 var t = [[0,0,0],
          [0,0,0],
-         [0,0,0]]; //Track array
+         [0,0,0]]; //Board Tracking Array. Inner array will be termed as the row array t[x]. The encasing array is the column array t[y]e.
 
 var l = [[0,0,0],
         [0,0,0],
-        [0,0,0]]; //Lock array; 0 = lock (user wont be able to change) 1 =
+        [0,0,0]]; //Lock array; 0: lock (user wont be able to change)
 
 
-var turn = 'a';// a->p1 / b->p2
-var move = 0;
-var start = 0;
+var turn = 'a';// a = plater 1 (p1)
+var move = 0; //One round has a maximum of  9 moves.
+var start = 0; //Tracks if game has started
 var promptSymbol = 0;
-var playerA, playerB; //To track player's choice of symbol(Ticker)
+var playerA, playerB; //To keep track player's choice of ticker(symbol)
 
 
 //-----AI-----
+
+// function aiTest() {
+//   var empty = [];
+//   var aiz = ['r'];
+//   var ii;
+//
+//   for (i=0; i<t.length; i++){
+//       for (j=0;j<t.length; j++){
+//         if (t[i][j]===0){
+//           aiz.push(i); aiz.push(j);
+//           ii=aiz.join('');
+//           empty.push(ii);
+//           aiz = ['r'];
+//         }
+//       }
+//     }
+//
+//   emptyCellsLeft = empty.length;
+//   emptyRandom = Math.floor((Math.random()*emptyCellsLeft));
+//
+//   ii = 0;// Reset
+//   empty = []// Reset
+// }
+
 
 //Senseless Random
 function ai(){
@@ -51,11 +75,6 @@ function ai(){
             selectEngine(id,true);
             }
       }
-
-
-
-
-
     // if (t[y][x]===0){
     //   playerToggle();
     //   t[y][x] = turn;
@@ -77,12 +96,12 @@ function ai(){
 function checkWin(){
   var check = '';//init check string to empty
   console.log('Checking Win...')
-  if(move === 9){
+  if(move === 9){ //checks if 9 moves have been made, if yes game is tied!
     document.getElementById("status").innerHTML = "It's a DRAW! Click \'Reset\' to replay the game :)";
   }
-  else if(move>=4){
+  else if(move>4){ //Only starts checking after 5 moves have been made!
       //---Horizontal Tally---
-        console.log('Horizontal Tallying!');
+        console.log('Horizontal Tallying!', t[0],t[1],t[2]);
         for(i=0; i<t.length; i++){
           console.log('Horizontal Tallying!');
           check = t[i].join('');
@@ -100,21 +119,18 @@ function checkWin(){
             else{
               check = '';
               console.log('No horizontals || Resetting check to:' , check);
-              console.log(t[0]);
-              console.log(t[1]);
-              console.log(t[2]);
             }
-            check = '';
+            check = ''; //
           }//End of  Horizontal Tally For-loop
 
 
         //---Vertical Tally---
         var y=[];//empty array for vertical tally
-        console.log('Vertical Tallying!');
+        console.log('Vertical Tallying!', t[0],t[1],t[2]);
         for(i=0;i<t.length;i++){ // i = column
           for(j=0;j<t.length;j++){ //j=row
             y.push(t[j][i]);
-            console.log('Horizontal Checking: ' + y);
+            console.log('Forming column: ' + y);
           }
           check = y.join('');
 
@@ -199,66 +215,52 @@ function checkWin(){
             console.log(t[2]);
             }
 
-    }//End of if(move>=4)
+    }//End of if(move<5)
     else{
-          console.log('To early to check... Check in ' + (4 - move) + ' more moves' );
+          console.log('To early to check... Check in ' + (5 - move) + ' more moves' );
           }
   }//checkWin() end
 
 //-----Function for Row 0 cells-----
-function selectr00(){
-  id = document.getElementById('r00').id
-  var idArr = id.split('');
-  var idx = idArr.pop(); var idy = idArr.pop();
+function gameButler(idx,idy){
 
+    var move1 = l[0].join('') + l[1].join('') + l[2].join('');
+    console.log(move1);
+    console.log("l[] array: ", l[0],l[1],l[2]);
     if(l[idy][idx]===0){
-      if(start === 0){
-            lock(idy,idx)
+        if(start === 0 && move1==='000000000') {
             chooseTicker(id);
+            lock(idy,idx);
             }
-           else if(start === 1){
-            selectEngine(id);
-            }
+
+        if(start === 1){
+          selectEngine(id);
+          lock(idy,idx);
+        }
       }
-      else{
-        alert("You shall not pass!");
+      else if(start===0 && l[idy][idx]!=0){
+        chooseTicker(id);
       }
+
+    }//End of gameButler()
+
+function selectr00(){
+  id = document.getElementById('r00').id; //obtains #id and stores in to variable id as string
+  var idArr = id.split(''); //splits string by characters and stores in an array, eg. [r,0,1]
+  var idx = idArr.pop(); var idy = idArr.pop(); //stores the digits seperately. idx will be used to traverse across the row array, vice versa
+  gameButler(idx,idy);
   }
 function selectr01(){
   id = document.getElementById('r01').id;
   var idArr = id.split('');
   var idx = idArr.pop(); var idy = idArr.pop();
-
-    if(l[idy][idx]===0){
-      if(start === 0){
-            lock(idy,idx)
-            chooseTicker(id);
-            }
-           else if(start === 1){
-            selectEngine(id);
-            }
-      }
-    else{
-        alert("Player A needs to select X or O first!");
-      }
+  gameButler(idx,idy);
 }
 function selectr02(){
   id = document.getElementById('r02').id;
   var idArr = id.split('');
   var idx = idArr.pop(); var idy = idArr.pop();
-
-    if(l[idy][idx]===0){
-      if(start === 0){
-            lock(idy,idx)
-            chooseTicker(id);
-            }
-           else if(start === 1){
-            selectEngine(id);
-            }
-      }
-    else{
-        alert("Player A needs to select X or O first!");
-      }
+  gameButler(idx,idy);
 }
 
 //-----Function for Row 1 cells-----
@@ -266,55 +268,19 @@ function selectr10(){
   id = document.getElementById('r10').id;
   var idArr = id.split('');
   var idx = idArr.pop(); var idy = idArr.pop();
-
-    if(l[idy][idx]===0){
-      if(start === 0){
-            lock(idy,idx)
-            chooseTicker(id);
-            }
-           else if(start === 1){
-            selectEngine(id);
-            }
-      }
-    else{
-        alert("Player A needs to select X or O first!");
-      }
+    gameButler(idx,idy);
 }
 function selectr11(){
   id = document.getElementById('r11').id;
   var idArr = id.split('');
   var idx = idArr.pop(); var idy = idArr.pop();
-
-    if(l[idy][idx]===0){
-      if(start === 0){
-            lock(idy,idx)
-            chooseTicker(id);
-            }
-           else if(start === 1){
-            selectEngine(id);
-            }
-      }
-    else{
-        alert("Player A needs to select X or O first!");
-      }
+  gameButler(idx,idy);
   }
 function selectr12(){
   id = document.getElementById('r12').id;
   var idArr = id.split('');
   var idx = idArr.pop(); var idy = idArr.pop();
-
-    if(l[idy][idx]===0){
-      if(start === 0){
-            lock(idy,idx)
-            chooseTicker(id);
-            }
-           else if(start === 1){
-            selectEngine(id);
-            }
-      }
-    else{
-        alert("Player A needs to select X or O first!");
-      }
+  gameButler(idx,idy);
   }
 
 //-----Function for Row 1 cells-----
@@ -322,67 +288,26 @@ function selectr20(){
   id = document.getElementById('r20').id;
   var idArr = id.split('');
   var idx = idArr.pop(); var idy = idArr.pop();
-
-    if(l[idy][idx]===0){
-      if(start === 0){
-            lock(idy,idx)
-            chooseTicker(id);
-            }
-           else if(start === 1){
-            selectEngine(id);
-            }
-      }
-    else{
-        alert("Player A needs to select X or O first!");
-      }
+  gameButler(idx,idy);
   }
 function selectr21(){
   id = document.getElementById('r21').id;
   var idArr = id.split('');
   var idx = idArr.pop(); var idy = idArr.pop();
-
-    if(l[idy][idx]===0){
-      if(start === 0){
-            lock(idy,idx)
-            chooseTicker(id);
-            }
-           else if(start === 1){
-            selectEngine(id);
-            }
-      }
-    else{
-        alert("Player A needs to select X or O first!");
-      }
+  gameButler(idx,idy);
   }
 function selectr22(){
   id = document.getElementById('r22').id;
   var idArr = id.split('');
   var idx = idArr.pop(); var idy = idArr.pop();
-
-    if(l[idy][idx]===0){
-      if(start === 0){
-            lock(idy,idx)
-            chooseTicker(id);
-            }
-           else if(start === 1){
-            selectEngine(id);
-            }
-      }
-    else{
-        alert("Player A needs to select X or O first!");
-      }
+  gameButler(idx,idy);
   }
 
 //-----Functions for player selction-----
 
 function lock(y,x){
-  for(i=0; i<l.length; i++){
-    for(j=0;j<l.length; j++){
-      l[i][j] = 1
-    }
+    l[y][x] = 1;
   }
-  l[y][x] = 0
-}
 
 function chooseTicker(id2check,aimode){
   if(aimode === true){
@@ -407,30 +332,37 @@ function start(){// linked to eventListener at button 'start'
   l = [[0,0,0],
       [0,0,0],
       [0,0,0]];
-  start = 1;
+  start = 1; move++;
   var g=['r'];
-  var ticker; //To store first player's ticker choice
+  var ticker; //Only to aid in finding out playerA's ticker(symbol) choice.
 
+  //Following nested For-loops checks the board where the first symbol was placed. If a symbol is found,
+  //var-ticker remember's playerA's choice then passed to the if/else to assign playerB's symbol and locks the cell.
   for(i=0; i<t.length; i++){
     for(j=0;j<t.length; j++){
       g.push(i); g.push(j);
-      id = g.join('');
-      ticker = document.getElementById(id).textContent;
+      id = g.join('');//Creates string to form #id
+      ticker = document.getElementById(id).textContent; // First symbol to show is always X
 
       if(ticker==='X'){
-        playerA = ticker;
+        playerA = ticker;//
         playerB = 'O'; //Stores ticker to variable playerA
         document.getElementById('status').innerHTML="<h1 id=\"status\">Player <span id=\"player\">B\'s turn!</span></h1>";
         turn = 'b';
+        l[i][j] = 1;
+        t[i][j] = 'a';
         }
       else if(ticker==='O') {
         playerA = 'O';
         playerB = 'X';
         document.getElementById('status').innerHTML="<h1 id=\"status\">Player <span id=\"player\">B\'s turn!</span></h1>";
         turn = 'b';
+        l[i][j] = 1;
+        t[i][j] = 'b';
         }
       g=['r'];
       }//End of j-For loop
+      console.log(l[i]);
     }//End of i-For loop
 }
 
@@ -480,9 +412,8 @@ function reset(){
   start = 0;
   promptSymbol = 0;
   move = 0;
-  t = [[0,0,0],
-       [0,0,0],
-       [0,0,0]];
+  t = [[0,0,0],[0,0,0],[0,0,0]];
+  l = [[0,0,0],[0,0,0],[0,0,0]];
   document.getElementById('status').innerHTML = '<h1 id="status">Player <span id="player">A\'s turn!</span></h1>';
 
   var z =['r'];
